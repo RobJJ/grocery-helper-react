8f7f3989c806f572c243a6e6106fa96a456d7476
RobJ <robjjohnsenwork@gmail.com>
2022-09-16 11:19:32 +0700

    Updates

---
+++ precache-manifest.1a8d95c8f8eaff2a94c0ae8a9228929e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76f73a81ba4ef8af715b66e090571ea",
+    "url": "/grocery-helper-react/index.html"
+  },
+  {
+    "revision": "9c74649d4cfd24d13aea",
+    "url": "/grocery-helper-react/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-helper-react/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-helper-react/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c74649d4cfd24d13aea",
+    "url": "/grocery-helper-react/static/js/main.1290f32e.chunk.js"
+  },
+  {
+    "revision": "4dbeae23b192b9289277",
+    "url": "/grocery-helper-react/static/js/runtime-main.830ffdc6.js"
+  }
+]);
